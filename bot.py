258a8e035d440081f6900aecb3699950cb272712
Celal (Cero) <65628815+CeroWalker@users.noreply.github.com>
2025-09-16 20:24:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,16 +15,4 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send(f'Merhaba! Ben {client.user}, bir Discord sohbet botuyum!')
 
-@client.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
 client.run("TOKEN")
